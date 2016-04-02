bd2aa08ea3df909bae3d30368009d7e8d11c944b
Stefan Motko <stefanmotko@gmail.com>
2016-04-02 15:28:53 +0200

    Modify application layout, add user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  get 'user/new'
+  get 'users/new'
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
