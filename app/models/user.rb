19091cd4117d0db41eba485402b992a210339f43
Stefan Motko <stefanmotko@gmail.com>
2016-04-09 11:14:38 +0200

    Added username, email and password constraints

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,13 @@ class User < ActiveRecord::Base
   has_many :favorites_memberships
   has_many :recipe_ratings
   has_many :ingredient_ratings
+
+  validates :username, length: {minimum: 3, maximum: 40}
+  validates :email, length: {maximum: 255}
+
   validates_uniqueness_of :username
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
   has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
 end
